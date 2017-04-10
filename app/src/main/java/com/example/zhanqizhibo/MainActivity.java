5d056852dd708a9ee73120cb9c50ad035aa735ea
AndroidZn <2313368759@qq.com>
2017-04-10 11:20:52 +0800

    toast

--- app/src/main/java/com/example/zhanqizhibo/MainActivity.java
+++ app/src/main/java/com/example/zhanqizhibo/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.zhanqizhibo;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(UpdateAddressActivity.this, "请输入姓名", Toast.LENGTH_LONG).show();
     }
+
 }
